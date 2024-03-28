11cb1044cf5432b56b9049cd3d214ca37f77d47c
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-03-28 11:47:52 +0100

    draw canvas and trash

--- dist/logicalcircuit.js
+++ dist/logicalcircuit.js
@@ -8,6 +8,18 @@ class LogicalCircuit {
   constructor() {
   }
 
+  get inputs() {
+    return this.#structure.inputs;
+  }
+
+  get operators() {
+    return this.#structure.operators;
+  }
+
+  get outputs() {
+    return this.#structure.outputs;
+  }
+
   addInput(name) {
     name = this.#purgeName(name);
     if (!name || this.#isNameAlreadyUsed(name)) {
@@ -86,15 +98,30 @@ class LogicalCircuit {
 class LogicalCircuitUI {
   #logicalCircuit;
   #canvas;
+  #ctx;
 
   constructor(container, options) {
     this.#logicalCircuit = new LogicalCircuit();
+
+    this.#canvas = document.createElement("canvas");
+    this.#canvas.classList.add("LogicalCircuitUI_Canvas");
+    this.#canvas.width = isNaN(options.width) || options.width < 0 ? 800 : options.width;
+    this.#canvas.height = isNaN(options.height) || options.height < 0 ? 600 : options.height;
+
+    container.append(this.#canvas);
+
+    this.#ctx = this.#canvas.getContext('2d');
+    this.#ctx.font = "24px sans-serif";
+    this.#ctx.textBaseline = "middle";
+    this.#ctx.lineWidth = 2;
+    this.#ctx.lineJoin = "round";
+    this.#draw();
   }
 
   addInput(name, top, left) {
     var added = this.#logicalCircuit.addInput(name);
     if (added) {
-      this.#addPosition(this.#logicalCircuit.#inputs, name, top, left);
+      this.#addPosition(this.#logicalCircuit.inputs, name, top, left);
       this.#draw();
     }
     return added;
@@ -103,7 +130,7 @@ class LogicalCircuitUI {
   addOutput(name, top, left) {
     var added = this.#logicalCircuit.addOutput(name);
     if (added) {
-      this.#addPosition(this.#logicalCircuit.#output, name, top, left);
+      this.#addPosition(this.#logicalCircuit.outputs, name, top, left);
       this.#draw();
     }
     return added;
@@ -111,49 +138,49 @@ class LogicalCircuitUI {
 
   addOR(top, left) {
     var name = this.#logicalCircuit.addOR();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addNOR(top, left) {
     var name = this.#logicalCircuit.addNOR();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addAND(top, left) {
     var name = this.#logicalCircuit.addAND();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addNAND(top, left) {
     var name = this.#logicalCircuit.addNAND();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addXOR(top, left) {
     var name = this.#logicalCircuit.addXOR();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addNXOR(top, left) {
     var name = this.#logicalCircuit.addNXOR();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
 
   addNOT(top, left) {
     var name = this.#logicalCircuit.addNOT();
-    this.#addPosition(this.#logicalCircuit.#operator, name, top, left);
+    this.#addPosition(this.#logicalCircuit.operators, name, top, left);
     this.#draw();
     return name;
   }
@@ -165,6 +192,28 @@ class LogicalCircuitUI {
   }
 
   #draw() {
+    this.#canvas.style.cursor = "default";
+    this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
 
+    this.#drawTrash();
+  }
+
+  #drawTrash() {
+    this.#ctx.font = "48px sans-serif";
+    this.#ctx.fillText("\u{1F5D1}", this.#canvas.width - 35, this.#canvas.height - 20);
+    this.#ctx.font = "24px sans-serif";
+
+    var gradient = this.#ctx.createRadialGradient(this.#canvas.width, this.#canvas.height, 40, this.#canvas.width, this.#canvas.height, 120);
+    gradient.addColorStop(0, "rgba(0,0,0,0)");
+    gradient.addColorStop(0.5, "rgba(0,0,0,0)");
+    gradient.addColorStop(1, "rgba(0,0,0,0.3)");
+
+    this.#ctx.lineWidth = 80;
+    this.#ctx.strokeStyle = gradient;
+    this.#ctx.beginPath();
+    this.#ctx.arc(this.#canvas.width, this.#canvas.height, 80, 0, 2 * Math.PI);
+    this.#ctx.stroke();
+    this.#ctx.lineWidth = 2;
+    this.#ctx.strokeStyle = "black";
   }
 }
