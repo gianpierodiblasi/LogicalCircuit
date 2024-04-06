26269b986a5322f5fa9f4c02145f4474e0905332
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-04-06 19:04:34 +0200

    refactoring

--- dist/logicalcircuit-ui.js
+++ dist/logicalcircuit-ui.js
@@ -1,3 +1,27 @@
 class LogicalCircuitUI {
-  
+  #core = new LogicalCircuitCore();
+  #jsonUI = {};
+
+  #default = {
+    "width": 800,
+    "height": 600,
+    "font": "24px sans-serif",
+    "lineWidth": 2,
+    "strokeStyle": "black",
+    "fillStyle": "black",
+    "cursor": "default",
+    "bezierConnector": false,
+    "showOperatorType": false,
+    "interactive": false
+  };
+
+  constructor(container, options) {
+    try {
+      options.width;
+    } catch (exception) {
+      options = {};
+    }
+    options.width = isNaN(options.width) || options.width < 0 ? this.#default.width : options.width;
+    options.height = isNaN(options.height) || options.height < 0 ? this.#default.height : options.height;
+  }
 }
